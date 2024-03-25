f6ddc4157fe939c3188a2a2f55a89db996346b2c
Alexis Berthelot <berthelota2@gmail.com>
2024-03-25 11:56:27 +0100

    maintenance

--- TourGuide/src/main/java/tourGuide/Application.java
+++ TourGuide/src/main/java/tourGuide/Application.java
@@ -3,6 +3,8 @@ package tourGuide;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//maintenance
+
 @SpringBootApplication
 public class Application {
 
